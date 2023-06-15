18289d5911068d769c0d8d557e3f9a9f9f8e311c
WEMAKWO <abateyrobespierre@gmail.com>
2023-06-15 11:33:59 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A C program that prints a line with put function
+ * Returrn: 0 (Passed)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
