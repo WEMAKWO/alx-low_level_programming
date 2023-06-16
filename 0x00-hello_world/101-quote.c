a5c91128ea6da54248c610e9dbb9f4726cf0e871
Wemakwo <abateyrobespierre@gmail.com>
2023-06-16 07:14:52 +0100

    101.quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 /**
  * main - A program that prints a line to an error
  * Return: 1 (Success)
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2,
+	       "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
 	return (1);
 }
