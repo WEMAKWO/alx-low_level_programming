2917e79c02021922842245ecd828eb2ea6af6ece
Wemakwo <abateyrobespierre@gmail.com>
2023-06-16 21:48:27 +0100

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'print lowercase'
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n = 97;
+
+	while
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
