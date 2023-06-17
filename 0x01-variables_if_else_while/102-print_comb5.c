c6ab3f1eb318e2c14a725b8f74f43ce24d213bce
WEMAKWO <abateyrobespierre@gmail.com>
2023-06-16 21:36:29 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'software'
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i, j;
+for (i = 0; i <= 99; i++)
+{
+int first_digit = i / 10;
+int second_digit = i % 10;
+for (j = 1; j <= 99; j++)
+{
+int third_digit = j / 10;
+int fourth_digit = j % 10;
+if (i <= j)
+{
+putchar('0' + first_digit);
+putchar('0' + second_digit);
+putchar(' ');
+putchar('0' + third_digit);
+putchar('0' + fourth_digit);
+if (i != 99 || j != 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
