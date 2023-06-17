87da964253be758f5ec367a391969b56a2365cd1
WEMAKWO <abateyrobespierre@gmail.com>
2023-06-16 20:58:30 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,16 +9,22 @@
 int main(void)
 {
 int i, j;
-for (i = 0; i <= 7; i++)
+for (i = 0; i <= 8; i++)
 {
-for (j = 0; j <= 7; j++)
+for (j = i + 1; j <= 9; j++)
 {
-putchar('0' + i);
-putchar('0' + j);
+int first_digit = i;
+int second_digit = j;
+putchar('0' + first_digit);
+putchar('0' + second_digit);
+if (i != 8 || j != 9)
+{
 putchar(',');
 putchar(' ');
 }
 }
+}
+
 putchar('\n');
 return (0);
 }
