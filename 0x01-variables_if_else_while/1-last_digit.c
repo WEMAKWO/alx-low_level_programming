3dc32de8365092b69866404c8a5337f99999fa34
WEMAKWO <abateyrobespierre@gmail.com>
2023-06-16 18:36:49 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,29 +5,22 @@
 /**
  * main - Entry point
  *
- * Return: 0
- *
+ * Return: Always 0
  */
 
 int main(void)
 {
-int n;
-int lastnum;
+	int n;
 
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-lastnum = n % 10;
-if (lastnum > 5)
-{
-printf("Last digit of %d is %d and greater than 5\n", n, lastnum);
-}
-else if (lastnum == 0)
-{
-printf("Last digit of %d is %d and is 0\n", n, lastnum);
-}
-else if (lastnum < 6 && lastnum != 0)
-{
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastnum);
-}
-return (0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	std::cout << "The last digit of " << n << " is ";
+
+    if (last_digit > 5) { std::cout << last_digit << " and is greater than 5" << std::endl;
+    } else if (last_digit == 0) {
+        std::cout << last_digit << " and is 0" << std::endl;
+    } else {
+        std::cout << last_digit << " and is less than 6 and not 0" << std::endl;
+    }
+	return (0);
 }
