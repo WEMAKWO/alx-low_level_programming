827cf4b411968bb500be25aa2f3dca4fa7207681
WEMAKWO <abateyrobespierre@gmail.com>
2023-07-28 10:52:53 -0700

    Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,7 +3,6 @@
 #include <stdio.h>
 #include "lists.h"
 
-
 /**
 * print_list - print the components of linked list
 * @h: pointer tto the list_t to be printed
@@ -11,21 +10,20 @@
 */
 size_t print_list(const list_t *h)
 {
-   int c = 0;
+	int c = 0;
 
-
-   while (h)
-   {
-       if (h->str)
-       {
-           printf("[0] (nil)\n");
-       }
-       else
-       {
-       printf("[%i] %s\n", h->len, h->str);
-       }
-       c++;
-       h = h->next;
-   }
-   return (c);
+	while (h)
+	{
+		if (h->str)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%i] %s\n", h->len, h->str);
+		}
+		c++;
+		h = h->next;
+	}
+	return (c);
 }
